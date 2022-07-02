67df7be0de87ae4eb729341695fcbfcd14792b63
Mo Kiu WONG <nicwongmk@gmail.com>
2022-07-03 00:46:20 +0800

    change gradeList

--- components/header/setting/gradeList.tsx
+++ components/header/setting/gradeList.tsx
@@ -12,6 +12,7 @@ const GradeList = () => {
         { grade: "C+", point: 2.3},
         { grade: "C", point: 2},
         { grade: "C-", point: 1.7},
+        { grade: "D+", point: 1.3},
         { grade: "D", point: 1},
         { grade: "D-", point: 0.7},
         { grade: "F", point: 0},
